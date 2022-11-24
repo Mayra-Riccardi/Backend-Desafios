fb9c99e39f8a0e195cefc81f7231c793442f085a
Mayra-Riccardi <may_ricca5@hotmail.com>
2022-11-24 00:05:16 -0300

    desafio 9

---
+++ DESAFIO 9/src/options/options.js
@@ -0,0 +1,13 @@
+import __dirname from "../utils.js";
+
+export const URLMongo = "mongodb://127.0.0.1mongodb+srv://mayricca5:mavica2105@ecommerce.68btp7z.mongodb.net/ecommerce?retryWrites=true&w=majority:27017/ecommerce";
+
+export const configMariaDB = {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : '',
+      database : 'ecommerce'
+    }
+}
